b0f3375643daec836be281cc818f1d24bd66bade
Brian <brian@grumpygator.us>
2016-10-13 15:32:12 -0400

    First Simple test file

---
+++ IMSATest.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 13 15:27:14 2016
+
+@author: brian
+"""
+
+import numpy as np
+import pandas as pd
+
+strDataPath = "N:\\Users\\brian\\Documents\\GitHub\\IMSA-Test-1\\Data\\2015-2016"
+
+print(strDataPath)
+
